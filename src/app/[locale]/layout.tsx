a5ef04ef292a460b190b00ef34e1751c8dbd1972
aibekovislam <islamxdaamduu@gmail.com>
2024-09-22 17:55:26 +0600

    changed logo to favicon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -35,7 +35,6 @@ export default function RootLayout(props: {
   children: React.ReactNode;
   params: { locale: string };
 }) {
-  // Validate that the incoming `locale` parameter is valid
   if (!AppConfig.locales.includes(props.params.locale)) notFound();
 
   return (
@@ -46,8 +45,3 @@ export default function RootLayout(props: {
     </html>
   );
 }
-
-// Enable edge runtime but you are required to disable the `migrate` function in `src/libs/DB.ts`
-// Unfortunately, this also means it will also disable the automatic migration of the database
-// And, you will have to manually migrate it with `drizzle-kit push`
-// export const runtime = 'edge';
